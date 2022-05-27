66dcbdae90bd834f3851c0da40e47b75bd576276
Michael <elTortuga@users.noreply.github.com>
2022-05-27 12:43:40 -0700

    started number 4

---
+++ Java/AbsoluteJava6thEd/Ch01/Programming Project 04/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        final double AMOUNT_OF_ARTIFICAL_SWEETENER_TO_KILL_MOUSE = .08; //https://www.doctorsbeyondmedicine.com/listing/aspartame-experiment
+        final double MOUSE_WEIGHT = 30; //grams
+        final double STARTING_WEIGHT = 75000;
+        final double DESIRED_WEIGHT = 70000;
+        final double FRACTION_OF_SWEETENER_TO_SODA = .001;
+
+         
+
+    }
+}
