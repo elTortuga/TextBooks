590c8d8609e169bdd16ad5cd6301168ce2ec0493
Michael <elTortuga@users.noreply.github.com>
2022-05-29 16:18:12 -0700

    completed number 4 from chapter 1

--- Java/AbsoluteJava6thEd/Ch01/Programming Project 04/src/App.java
+++ Java/AbsoluteJava6thEd/Ch01/Programming Project 04/src/App.java
@@ -2,11 +2,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         final double AMOUNT_OF_ARTIFICAL_SWEETENER_TO_KILL_MOUSE = .08; //https://www.doctorsbeyondmedicine.com/listing/aspartame-experiment
         final double MOUSE_WEIGHT = 30; //grams
-        final double STARTING_WEIGHT = 75000;
+        // final double STARTING_WEIGHT = 75000; // not necessary
         final double DESIRED_WEIGHT = 70000;
         final double FRACTION_OF_SWEETENER_TO_SODA = .001;
 
-         
-
+        System.out.println("The amount of soda a day in grams is "+
+                    AMOUNT_OF_ARTIFICAL_SWEETENER_TO_KILL_MOUSE*DESIRED_WEIGHT /
+                    (MOUSE_WEIGHT*FRACTION_OF_SWEETENER_TO_SODA) +
+                    "\nthats about " + 
+                    (int)(AMOUNT_OF_ARTIFICAL_SWEETENER_TO_KILL_MOUSE*DESIRED_WEIGHT /
+                    (MOUSE_WEIGHT*FRACTION_OF_SWEETENER_TO_SODA)) / 1000 +
+                    " liters a day");
     }
 }
