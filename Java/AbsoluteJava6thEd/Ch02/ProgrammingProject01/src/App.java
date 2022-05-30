a3cd87dc4fedd734aae29d111b3e3f4fbe1b390e
Michael <elTortuga@users.noreply.github.com>
2022-05-29 19:23:10 -0700

    Started chapter 2

---
+++ Java/AbsoluteJava6thEd/Ch02/ProgrammingProject01/src/App.java
@@ -0,0 +1,88 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double n = 54756;
+        System.out.println("n = " + n);
+        
+        double guess = n/2;
+        double r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+        
+        r = n/guess;
+        guess = (guess+r) / 2;
+        System.out.println("guess = " + guess);
+        System.out.println("r = " + r);
+    }
+}
+
+/*OUTPUT:
+n = 54756.0
+guess = 13690.0
+r = 2.0
+guess = 6846.9998539079625
+r = 3.9997078159240322
+guess = 3427.49846654565
+r = 7.997079183337169
+guess = 1721.7369830172013
+r = 15.975499488752497
+guess = 876.7698749777658
+r = 31.802766938330297
+guess = 469.61091910771336
+r = 62.451963237660934
+guess = 293.1047854128459
+r = 116.5986517179784
+guess = 239.95926070224007
+r = 186.81373599163425
+guess = 234.07399753610952
+r = 228.188734369979
+guess = 234.00001169637682
+r = 233.9260258566441
+guess = 234.00000000000028
+r = 233.99998830362378
+*/
