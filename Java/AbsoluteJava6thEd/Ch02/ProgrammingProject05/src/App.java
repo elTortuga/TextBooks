3ca2c4b3ab618034b871633fa1816b1b604ea902
Michael <elTortuga@users.noreply.github.com>
2022-06-07 16:17:45 -0700

    finished PP5

---
+++ Java/AbsoluteJava6thEd/Ch02/ProgrammingProject05/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboardInput = new Scanner(System.in);
+        System.out.print("Enter the percentage: ");
+        float percentage = Float.parseFloat(keyboardInput.nextLine());
+        float gpa = percentage * 4 / 100;
+        System.out.printf("The gpa is %.0f", gpa);
+
+
+    }
+}
